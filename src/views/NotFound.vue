06b3637b5b1fe0b46043e495acd759c0024e9c84
Artem <art_nazarov@mail.ru>
2021-03-15 10:09:26 +0300

    notepad init

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>404: Page Not Found</h1>
+
+  <a href="/"><button>Return to Home </button></a>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage",
+};
+</script>
+
+<style>
+</style>
